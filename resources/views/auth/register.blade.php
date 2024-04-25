1ce2750c5451c28ab1b1bf5f940862c6d61aa632
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2024-04-25 23:11:15 +0600

    design Login Register pages

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,157 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="{{asset('/')}}assets/auth//css/styles.css">
+    <title>SignUp</title>
+    <style>
+        .Section{
+            background-image: url("{{asset('/')}}assets/auth//img/3.jpg");
+            background-size: cover;
+            background-repeat: no-repeat;
+            height: 100vh;
+        }
+        .pt{
+            padding-top: 12px;
+        }
+        .back{
+            text-align: center;
+            padding-top: 19px;
+        }
+    </style>
+</head>
+<body>
+    <div class="Section">
+
+        <div class="wrapper">
+            <h4 class="text-center ">
+                <?php echo isset($message) ? $message : '';  ?>
+            </h4>
+
+            <div class="title">
+                SignUp Here
+            </div>
+
+            <div class="social_media">
+                <div class="item">
+                    <i class="fab fa-facebook-f"></i>
+                </div>
+                <div class="item">
+                    <i class="fab fa-twitter"></i>
+                </div>
+                <div class="item">
+                    <i class="fab fa-google-plus-g"></i>
+                </div>
+            </div>
+
+            <form class="form pt" method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
+                @csrf
+                <div class="input_field">
+                    <input type="text" placeholder="    Enter Your Name" name="name" class="input" id="" value="{{old('name')}}" onkeyup="validateName()">
+                    @error('name')
+                        <span style="color: red">{{ $message }}</span>
+                    @enderror
+                    <i class="fas fa-user"></i>
+                    <samp  id="name-error"></samp>
+                </div>
+
+                <div class="input_field">
+                    <input type="text" placeholder="    Enter Your Email" name="email" value="{{old('email')}}"  class="input" id="" onkeyup="validateEmail()">
+                    @error('email')
+                        <span style="color: red">{{$message}}</span>
+                    @enderror
+                    <i class="fas fa-envelope"></i>
+
+                </div>
+                <div class="input_field">
+                    <input type="password" placeholder="    Enter Your Password" name="password" value="{{old('password')}}"  class="input" id="" onkeyup="validatePass()" >
+                    @error('password')
+                        <span style="color: red">{{$message}}</span>
+                    @enderror
+                    <i class="fas fa-lock"></i>
+                    <samp id="Pass-error"></samp>
+                </div>
+                <div class="input_field">
+                    <input type="password" placeholder="    Enter Your Confm Password" name="password_confirmation" value="{{old('password_confirmation')}}"  class="input" id="" onkeyup="validateConPass()">
+                    @error('password_confirmation')
+                        <span style="color: red">{{$message}}</span>
+                    @enderror
+                    <i class="fas fa-lock"></i>
+                    <samp  id="conPass-error"></samp>
+                </div>
+
+
+                <div class="border-0">
+                <button type="submit" class="btn" name="signBtn" >SignUp</button>
+                </div>
+                <div class="back"><a  href="{{ route('home') }}">Go To Home</a></div>
+            </form>
+
+        </div>
+    </div>
+
+<script src="{{asset('/')}}assets/auth//js/scripts.js"></script>
+</body>
+
+
+</html>
+
+
+
+
+{{-- <x-guest-layout>
+    <form method="POST" action="{{ route('register') }}">
+        @csrf
+
+        <!-- Name -->
+        <div>
+            <x-input-label for="name" :value="__('Name')" />
+            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div>
+
+        <!-- Email Address -->
+        <div class="mt-4">
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Confirm Password -->
+        <div class="mt-4">
+            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+                            type="password"
+                            name="password_confirmation" required autocomplete="new-password" />
+
+            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('Already registered?') }}
+            </a>
+
+            <x-primary-button class="ml-4">
+                {{ __('Register') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout> --}}
